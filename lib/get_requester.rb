a86323996a22d88f1ad6144a612a97fd5aca8fb7
Shantel <Shantelgray24@gmail.com>
2025-08-03 15:46:06 -0400

    Remote Data Lab

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,22 @@
-# Write your code here
+require 'json'
+require 'net/http'
+require 'uri'
+
+class GetRequester
+  def initialize(url)
+    @url = url
+  end 
+
+  def parse_json
+  appliances = JSON.parse(get_response_body)
+  end
+
+ def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+end
+
+#appliances = GetRequester.new(url)
+#puts appliances.program_appliances.uniq
